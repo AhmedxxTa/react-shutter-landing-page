6b29f30c5da772e40456968134bc7831e9ef5d95
Ahmed <a.tamer87@yahoo.com>
2022-05-11 06:22:26 +0200

    Added Navbar And Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "./Navbar";
+import heroImg from "../images/video-lighting.jpg";
+
+const Hero = () => {
+  return (
+    <div className="h-screen ">
+      <div>
+        <img
+          className="object-cover h-full w-full absolute -z-10"
+          src={heroImg}
+          alt=""
+        />
+      </div>
+
+      <div className="w-full max-w-[650px] h-[100%] flex flex-col justify-center items-start text-center text-white px-12">
+        <h1 className="text-[2rem] font-bold">Shutter &amp; Shoot</h1>
+        <p className="text-lg text-left mb-8">
+          A Professional Studio For more than 20 years. Featuring One of the
+          Most Famous Camera Staff In the world. 24/7 service. Ready to Shoot
+          you whenever you wish.
+        </p>
+        <button className="text-2xl text-white py-4 px-8 mb-10 bg-[#f76d28] border-[2px] border-[#f76d28] rounded-lg transition-[0.2s] ease-in-out hover:bg-[#f76d28] hover:scale-105 active:scale-95 ">Book Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { MdOutlineClose } from "react-icons/md";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <nav className="bg-gradient-to-b from-[rgba(0,0,0,0.8)] to-[rgba(0,0,0,0.2)] bg-cover bg-fixed bg-center w-full h-20 flex justify-between items-center mx-auto p-8 font-bold fixed duration-1000">
+      
+      <div onClick={handleNav} >
+        {!nav ? (
+          <FaBars size={30} className="block absolute top-0 left-0 text-white cursor-pointer translate-x-2/4 translate-y-3/4 font-bold text-[2rem]" />
+        ) : (
+          <MdOutlineClose size={30} className="block absolute top-0 left-0 text-white cursor-pointer translate-x-2/4 translate-y-3/4 font-bold text-[2rem]" />
+        )}
+      </div>
+
+      {/* sidebar */}
+      <div className={nav ? "w-[450px] h-screen bg-black text-white absolute top-[5px] left-0 flex justify-center items-center text-start duration-500 ease-in-out -z-20" : "w-[450px] h-screen text-white absolute flex justify-center items-center text-start -left-full top-[-5px] duration-500 ease-in"}>
+          <ul>
+            <li className="text-2xl opacity-80 hover:opacity-100 cursor-pointer"><a href="#about">About Us</a></li>
+            <li className="text-2xl opacity-80 hover:opacity-100 cursor-pointer"><a href="#staff">Staff</a></li>
+            <li className="text-2xl opacity-80 hover:opacity-100 cursor-pointer"><a href="#ourWork">Our Work</a></li>
+            <li className="text-2xl opacity-80 hover:opacity-100 cursor-pointer"><a href="#contact">Contact</a></li>
+            <button className="mt-6 text-xl text-white py-3 px-7 mb-10 bg-[#f76d28] border-[2px] border-[#f76d28] rounded-lg transition-[0.2s] ease-in-out hover:bg-[#f76d28] hover:scale-105 active:scale-95 ">Book Now</button>
+          </ul>
+        </div>
+
+      <div className="text-[2rem] text-white hidden md:flex justify-center items-center px-4 lg:translate-x-1/4">
+        <Link to="/">
+          <h1>Shutter &amp; Shoot Studio</h1>
+        </Link>
+      </div>
+      <div className="text-lg text-white hidden lg:flex items-center justify-center gap-4">
+          <ul className="flex text-white items-center cursor-pointer">
+            <li className="opacity-80 hover:opacity-100 mr-2"><a href="#about">About Us</a></li>
+            <li className="opacity-80 hover:opacity-100 mr-2"><a href="#staff">Staff</a></li>
+            <li className="opacity-80 hover:opacity-100 mr-2"><a href="#ourWork">Our Work</a></li>
+            <li className="opacity-80 hover:opacity-100 mr-2"><a href="#contact">Contact</a></li>
+          </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
